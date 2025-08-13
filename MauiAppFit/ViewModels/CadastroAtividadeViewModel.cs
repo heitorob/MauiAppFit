5e56cb01a7ed0d6b66c9914a62e722140ed204e0
231009 <231009@CPD02-M12>
2025-08-13 14:58:09 -0300

    Enviei, Jailton. Finalizada a view "CadastroAtividade.xaml" e iniciada a view-model "CadastroAtividadeViewModel.cs". Agora, deixe de ser sem-vergonha.

--- MauiAppFit/ViewModels/CadastroAtividadeViewModel.cs
+++ MauiAppFit/ViewModels/CadastroAtividadeViewModel.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiAppFit.Models;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MauiAppFit.ViewModels
 {
-    internal class CadastroAtividadeViewModel
+    [QueryProperty("PegarIdNavegacao", "parametro_id")]
+    public class CadastroAtividadeViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string descricao, observacoes;
+        int id;
+        DateTime data;
+        double? peso;
+
+        public string PegarIdNavegacao
+        {
+            set
+            {
+                int id_parametro = Convert.ToInt32(Uri.UnescapeDataString(value));
+                VerAtividade.Execute(id_parametro);
+            }
+        }
+
+        public string Descricao
+        {
+            get => descricao;
+            set
+            {
+                descricao = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Descricao"));
+            }
+        }
+
+        public string Observacoes
+        {
+            get => observacoes;
+            set
+            {
+                observacoes = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Observacoes"));
+            }
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+            }
+        }
+
+        public DateTime Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Data"));
+            }
+        }
+
+        public double? Peso
+        {
+            get => peso;
+            set
+            {
+                peso = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Peso"));
+            }
+        }
+
+        public ICommand NovaAtividade
+        {
+
+        }
     }
 }
